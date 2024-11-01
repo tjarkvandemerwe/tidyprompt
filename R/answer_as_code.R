4801399037c562c493d4a429642371f5694f0237
lukakoning <koningluka@gmail.com>
2024-11-01 16:46:06 +0100

    Update answer_as_code.R

--- R/answer_as_code.R
+++ R/answer_as_code.R
@@ -37,6 +37,16 @@
 #'  answer_as_integer() |>
 #'  answer_by_chain_of_thought() |>
 #'  send_prompt()
+#'
+#'  paste0(
+#'  "Using the built-in airquality dataset in R,",
+#'  " calculate the average Ozone level for days in May with temperatures",
+#'  " above 80°F."
+#'  ) |>
+#'   answer_as_integer() |>
+#'   answer_as_code(pkgs_to_use = c("dplyr")) |>
+#'   answer_by_chain_of_thought() |>
+#'   send_prompt()
 #' }
 answer_as_code <- function(
     prompt,
