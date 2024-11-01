bc9a0dc6d65ee262853b7798b2c9d9c72c2ceac9
lukakoning <koningluka@gmail.com>
2024-11-01 17:21:29 +0100

    plot example

--- dev_playground/test answer as code.R
+++ dev_playground/test answer as code.R
@@ -17,11 +17,13 @@ paste0(
   answer_by_chain_of_thought() |>
   send_prompt()
 
-plot_code <- glue::glue(
+plot <- glue::glue(
   "Using ggplot2, create a scatter plot of miles per gallon (mpg) versus horsepower (hp) for the cars in the mtcars dataset. Use different colors to represent the number of cylinders (cyl)."
-) |> as.character() |>
+) |>
   answer_as_code(
     pkgs_to_use = c("ggplot2"), evaluate_code = FALSE, send_back_output = FALSE
   ) |>
-  answer_by_chain_of_thought() |>
-  send_prompt()
+  answer_by_chain_of_thought(extract_from_finish_brackets = FALSE) |>
+  send_prompt() |>
+  eval()
+plot
