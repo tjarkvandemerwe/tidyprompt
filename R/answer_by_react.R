3e98eeb7fbffbbe94f862d56e04bef7e3293a8da
lukakoning <102363211+lukakoning@users.noreply.github.com>
2024-11-14 15:40:11 +0100

    add answer by react

---
+++ R/answer_by_react.R
@@ -0,0 +1,65 @@
+#' Set ReAct mode for a prompt
+#'
+#' @details Please note that ReAct mode may be most useful if in combination
+#' with tools that the LLM can use. See, for example, 'add_tools()' for
+#' enabling R function calling, or, for example, 'answer_as_code()' with
+#' 'output_as_tool = TRUE' for enabling R code evaluation as a tool.
+#'
+#' @param prompt A tidyprompt object or a single string
+#' @param extract_from_finish_brackets A logical indicating whether the final answer
+#' should be extracted from the text inside the "FINISH[...]" brackets.
+#'
+#' @return A tidyprompt with the ReAct-mode prompt_wrap added.
+#' @export
+answer_by_react <- function(
+    prompt,
+    extract_from_finish_brackets = TRUE
+) {
+
+  # Define modification/extraction/validation functions:
+  modify_fn <- function(original_prompt_text) {
+    new_prompt <- glue::glue(
+      "You are given a user's prompt.
+        To answer the user's prompt, you need to think step by step, take an action if needed, and then return a final answer.
+
+        ----- START OF USER'S PROMPT -----
+        {original_prompt_text}
+        ----- END OF USER'S PROMPT -----
+
+        Use the following structure:
+          Thought: <describe your thought process>
+          Action: <if needed, describe the action you take (e.g., look up information)>
+          Observation: <describe the result or observation from the action>
+          (Repeat Thought -> Action -> Observation as necessary)
+
+          When you are done, you must type:
+          FINISH[<put here your final answer to the user's prompt>]
+
+          Ensure your final answer aligns with your reasoning and observations."
+    )
+
+    return(new_prompt)
+  }
+
+  extraction_fn <- function(llm_response) {
+    if (!extract_from_finish_brackets) {
+      return(llm_response)
+    }
+    extracted_response <- stringr::str_extract(llm_response, "(?si)(?<=FINISH\\[).+?(?=\\])")
+
+    if (
+      is.na(extracted_response) ||
+      tolower(extracted_response) == "answer" ||
+      tolower(extracted_response) == "final answer"
+    ) {
+      return(create_llm_feedback(glue::glue(
+        "Error, could not parse your final answer.
+          Please type: 'FINISH[<put here your final answer to the original prompt>]'"
+      )))
+    }
+
+    return(extracted_response)
+  }
+
+  prompt_wrap(prompt, modify_fn, extraction_fn, type = "mode")
+}
