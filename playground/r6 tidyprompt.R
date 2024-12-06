71e76cefa6c5029373d109c85a9209a63fa8e298
lukakoning <koningluka@gmail.com>
2024-12-06 09:25:14 +0100

    Create r6 tidyprompt.R

---
+++ playground/r6 tidyprompt.R
@@ -0,0 +1,47 @@
+tp <- R6::R6Class(
+  "tp",
+
+  public = list(
+    prompt = NULL,
+    modify_fn = NULL,
+    my_other_field = " hi from tp object",
+
+    initialize = function(prompt = "Hi!") {
+      self$prompt <- prompt
+    },
+
+    construct_prompt = function() {
+      prompt <- self$prompt
+
+      if (!is.null(self$modify_fn)) {
+        # Add `self` to the environment of `modify_fn`
+        environment(self$modify_fn) <- list2env(list(self = self), parent = environment(self$modify_fn))
+        prompt <- self$modify_fn(prompt)
+      }
+
+      return(prompt)
+    }
+  )
+)
+
+
+tpi <- tp$new()
+# tpi$modify_fn <- function(prompt) {
+#   paste0(prompt, " How are you?")
+# }
+# tpi$construct_prompt()
+
+
+pw <- function(tpi, text = " hi from wrap function env!") {
+  tpi <- tpi$clone()
+
+  tpi$modify_fn <- function(prompt) {
+    paste0(prompt, text, self$my_other_field)
+  }
+
+  return(tpi)
+}
+
+tpi <- pw(tpi)
+tpi$construct_prompt()
+
