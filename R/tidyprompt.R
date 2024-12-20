da857cc8c0389587991425bad3bb1323b2017dfd
lukakoning <koningluka@gmail.com>
2024-12-20 17:30:29 +0100

    move system prompt

--- R/tidyprompt.R
+++ R/tidyprompt.R
@@ -449,7 +449,7 @@ construct_prompt_text <- function(x, llm_provider = NULL) {
 
 
 #' @title
-#' Set the chat history
+#' Set the chat history of a [tidyprompt-class] object
 #'
 #' @description
 #' This function sets the chat history for a [tidyprompt-class] object.
@@ -501,4 +501,39 @@ set_chat_history <- function(x, chat_history) {
 get_chat_history <- function(x) {
   x <- tidyprompt(x)
   x$get_chat_history()
+}
+
+
+
+#' Set system prompt of a [tidyprompt-class] object
+#'
+#' Set the system prompt for a prompt. The system prompt will be added
+#' as a message with role 'system' at the start of the chat history when
+#' this prompt is evaluated by [send_prompt()].
+#'
+#' @details The system prompt will be stored in the [tidyprompt()] object
+#' as '$system_prompt'.
+#'
+#' @param prompt A single string or a [tidyprompt()] object
+#' @param system_prompt A single character string representing the system prompt
+#'
+#' @return A [tidyprompt()] with the system prompt set
+#'
+#' @export
+#'
+#' @example inst/examples/set_system_prompt.R
+#'
+#' @family pre_built_prompt_wraps
+#' @family miscellaneous_prompt_wraps
+set_system_prompt <- function(prompt, system_prompt) {
+  prompt <- tidyprompt(prompt)
+
+  if (!is.character(system_prompt) |
+      length(system_prompt) != 1
+  )
+    stop("system_prompt must be a single character string")
+
+  prompt$system_prompt <- system_prompt
+
+  return(prompt)
 }
